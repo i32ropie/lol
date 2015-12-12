6f1736cb1e71abc8ccbd70fb9fe1fd4f90da4cfa
i32ropie <i32ropie@uco.es>
2015-12-12 17:59:54 +0100

    Custom commit

--- plugins/usuarios.py
+++ plugins/usuarios.py
@@ -32,5 +32,5 @@ def command_usuarios(m):
                     y[3] += 1
         txt = "*Usuarios*: " + str(x[0]) + "\n *-*Español: _" + str(x[1]) + "_\n *-*Inglés: _" \
         + str(x[2]) + "_\n *-*Italiano: _" + str(x[3]) + "_\n*Grupos*: " + str(y[0]) + \
-        "\n *-*Español: _" + str(y[1]) + "_\n *-*Inglés: _" + str(y[2]) + "_\n *-*Italiano: _" + str(y[3])
+        "\n *-*Español: _" + str(y[1]) + "_\n *-*Inglés: _" + str(y[2]) + "_\n *-*Italiano: _" + str(y[3]) + "_"
         bot.send_message( cid, txt, parse_mode="Markdown")
