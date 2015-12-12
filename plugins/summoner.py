f2854dadadd2cae8a60f4a365211832bbfea34c7
i32ropie <i32ropie@uco.es>
2015-12-12 17:44:16 +0100

    Custom commit

--- plugins/summoner.py
+++ plugins/summoner.py
@@ -21,3 +21,29 @@ def command_summoner(m):
         bot.send_message(cid, txt, parse_mode="Markdown")
     else:
         bot.send_message( cid, responses['not_user'])
+
+
+@bot.message_handler( func=lambda message: message.text.lstrip('/').split(' ')[0].split('@')[0] in ['euw','eune','br','na','las','lan','kr','tr','ru','oce'] )
+def region(m):
+    cid = m.chat.id
+    uid = m.from_user.id
+    if is_banned(uid):
+        if not extra['muted']:
+            bot.reply_to( m, responses['banned'])
+        return None
+    if is_user(cid):
+        invocador = ' '.join(m.text.split(' ')[1:])
+        if not invocador:
+            bot.send_message( cid, responses['no_summoner'][lang(cid)])
+        
+    else:
+        bot.send_message( cid, responses['not_user'])
+
+
+
+
+
+
+
+def region(txt):
+    command = txt.lstrip('/')
--- plugins/usuarios.py
+++ plugins/usuarios.py
@@ -2,20 +2,25 @@
 
 from config import *
 
-print(Color('{autored}[{/red}{autoyellow}+{/yellow}{autored}]{/red} {autocyan}  usuarios.py importado.{/cyan}'))
+print(Color('{autored}[{/red}{autoyellow}+{/yellow}{autored}]{/red} {autocyan}  stats.py importado.{/cyan}'))
 
-@bot.message_handler(commands=['usuarios'])
+@bot.message_handler(commands=['stats'])
 def command_usuarios(m):
     cid = m.chat.id
     uid = m.from_user.id
     if not is_recent(m):
         return None
     if is_admin(uid):
-        x = 0
-        y = 0
+        normal_chat, group_chat, es, en, it = 0, 0, 0, 0, 0
         for uid in users:
             if int(uid) > 0:
-                x += 1
+                normal_chat += 1
             else:
-                y += 1
-        bot.send_message( cid, '*>*Users: _%s_\n*>*Groups: _%s_' %(x,y), parse_mode="Markdown")
+                group_chat += 1
+            if lang(uid) == 'es':
+                es += 1
+            elif lang(uid) == 'en':
+                en += 1
+            elif lang(uid) == 'it':
+                it += 1
+        bot.send_message( cid, '*>*Usuarios: _%s_\n*>*Grupos: _%s_\n*>*Español: _%s_\n*>*Inglés: _%s_\n*>*Italiano: _%s_' %(normal_chat, group_chat, es, en, it), parse_mode="Markdown")
