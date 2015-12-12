e64e48547a09613ad13e00e9f0d5a35c0ac10588
i32ropie <i32ropie@uco.es>
2015-12-12 13:25:06 +0100

    Custom commit

--- plugins/ping.py
+++ plugins/ping.py
@@ -7,7 +7,7 @@ print(Color('{autored}[{/red}{autoyellow}+{/yellow}{autored}]{/red} {autocyan}  
 @bot.message_handler(commands=['ping'])
 def command_COMANDO(m):
     cid = m.chat.id
-    uid = m.chat.id
+    uid = m.from_user.id
     if not is_recent(m):
         return None
     if is_admin(uid):
