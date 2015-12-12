21c964cff1ab1868d3bec8e05bfa0d7fa6405dec
i32ropie <i32ropie@uco.es>
2015-12-12 17:58:58 +0100

    Custom commit

--- plugins/usuarios.py
+++ plugins/usuarios.py
@@ -31,6 +31,6 @@ def command_usuarios(m):
                 elif lang(uid) == 'it':
                     y[3] += 1
         txt = "*Usuarios*: " + str(x[0]) + "\n *-*Español: _" + str(x[1]) + "_\n *-*Inglés: _" \
-        + str(x[2]) + "_\n *-*Italiano: _" + str(x[3]) + "*Grupos*: " + str(y[0]) + \
+        + str(x[2]) + "_\n *-*Italiano: _" + str(x[3]) + "_\n*Grupos*: " + str(y[0]) + \
         "\n *-*Español: _" + str(y[1]) + "_\n *-*Inglés: _" + str(y[2]) + "_\n *-*Italiano: _" + str(y[3])
         bot.send_message( cid, txt, parse_mode="Markdown")
