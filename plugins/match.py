48b0914d6d1372972397c4a225b2529e3e492695
i32ropie <i32ropie@uco.es>
2015-12-12 16:46:03 +0100

    Custom commit

---
+++ plugins/match.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from config import *
+
+print(Color('{autored}[{/red}{autoyellow}+{/yellow}{autored}]{/red} {autocyan}  match.py importado.{/cyan}'))
+
+@bot.message_handler( func=lambda message: message.text=="PARTIDA" or message.text=="MATCH" or message.text=="PARTITA" )
+@bot.message_handler(commands=['match'])
+def command_match(m):
+    cid = m.chat.id
+    uid = m.from_user.id
+    if is_banned(uid):
+        if not extra['muted']:
+            bot.reply_to( m, responses['banned'])
+        return None
+    if is_user(cid):
+        txt = responses['match_1'][lang(cid)]
+        for region in ['euw','eune','br','na','las','lan','kr','tr','ru','oce']:
+            txt += '\n/' + region + responses['match_3'][lang(cid)] + '*' + region.upper() + '*'
+        txt += responses['match_2'][lang(cid)]
+        bot.send_message(cid, txt, parse_mode="Markdown")
+    else:
+        bot.send_message( cid, responses['not_user'])
---
+++ plugins/summoner.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from config import *
+
+print(Color('{autored}[{/red}{autoyellow}+{/yellow}{autored}]{/red} {autocyan}  summoner.py importado.{/cyan}'))
+
+@bot.message_handler( func=lambda message: message.text=="INVOCADOR" or message.text=="SUMMONER" or message.text=="EVOCATORE" )
+@bot.message_handler(commands=['summoner'])
+def command_summoner(m):
+    cid = m.chat.id
+    uid = m.from_user.id
+    if is_banned(uid):
+        if not extra['muted']:
+            bot.reply_to( m, responses['banned'])
+        return None
+    if is_user(cid):
+        txt = responses['summoner_1'][lang(cid)]
+        for region in ['euw','eune','br','na','las','lan','kr','tr','ru','oce']:
+            txt += '\n/' + region + responses['summoner_3'][lang(cid)] + '*' + region.upper() + '*'
+        txt += responses['summoner_2'][lang(cid)]
+        bot.send_message(cid, txt, parse_mode="Markdown")
+    else:
+        bot.send_message( cid, responses['not_user'])
