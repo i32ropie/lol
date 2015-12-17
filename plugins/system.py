0a5f6c2ff0c4eaf0da0ffa8aca92d98c1da1b3c8
i32ropie <i32ropie@uco.es>
2015-12-17 02:47:48 +0100

    Custom commit

--- plugins/system.py
+++ plugins/system.py
@@ -2,7 +2,6 @@
 
 from config import *
 import platform, psutil, subprocess
-from datetime import timedelta
 
 print(Color('{autored}[{/red}{autoyellow}+{/yellow}{autored}]{/red} {autocyan}  rotation.py importado.{/cyan}'))
 
@@ -11,9 +10,7 @@ def command_system(m):
     cid = m.chat.id
     uid = m.from_user.id
     if is_admin(uid):
-        with open('/proc/uptime', 'r') as f:
-            uptime_seconds = float(f.readline().split()[0])
-            uptime_string = str(timedelta(seconds = uptime_seconds))[:-7]
+        uptime = subprocess.getoutput('uptime')
         running = '\n*Running on*:\n'
         running += '\tSystem: {0}\n'.format(subprocess.getoutput('head -n1 /etc/issue | cut -d " " -f -3'))
         running += '\tKerne: {0}\n'.format(subprocess.getoutput('uname -rs'))
@@ -21,5 +18,5 @@ def command_system(m):
         running += '\tRAM: {0}MB ({1}% used)\n'.format(int(psutil.virtual_memory()[0] / 1000 / 1000), psutil.virtual_memory()[2])
         running += '\tPython: {0} ({1})\n'.format(str(platform.python_version()), str(platform.python_compiler()))
         running += '\tServer time: {0}\n'.format(time.strftime("%c"))
-        running += '\tUptime: Server up for {0}\n'.format(uptime_string)
+        running += '\tUptime: Server up for {0} days, {1} hours, {2} minutes\n'.format(uptime.split()[2], uptime.split(':')[0], uptime.split(':')[1])
         bot.send_message( cid, running, parse_mode="Markdown")
