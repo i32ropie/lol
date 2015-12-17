01f595f2fb967a7058f6874a21e0708a25bd7690
i32ropie <i32ropie@uco.es>
2015-12-17 02:30:11 +0100

    Custom commit

--- plugins/system.py
+++ plugins/system.py
@@ -11,11 +11,11 @@ def command_system(m):
     uid = m.from_user.id
     if is_admin(uid):
         running = '\n*Running on*:\n'
-        running += '\t🖥 {0}\n'.format(subprocess.getoutput('head -n1 /etc/issue | cut -d " " -f -3'))
-        running += '\t💾 {0}\n'.format(subprocess.getoutput('uname -rs'))
-        running += '\t💡 {0}\n'.format(subprocess.getoutput('cat /proc/cpuinfo | grep "model name" | tr -s " " | cut -d " " -f 3-'))
-        running += '\t🗃 {0}MB ({1}% used)\n'.format(int(psutil.virtual_memory()[0] / 1000 / 1000), psutil.virtual_memory()[2])
-        running += '\t🐍 Python {0} ({1})\n'.format(str(platform.python_version()), str(platform.python_compiler()))
-        running += '\t⌚️ {0}\n'.format(time.strftime("%c"))
-        running += '\t⏱ {0}\n'.format(subprocess.getoutput('uptime -p'))
-        bot.send_message( cid, running)
+        running += '\tSystem: {0}\n'.format(subprocess.getoutput('head -n1 /etc/issue | cut -d " " -f -3'))
+        running += '\tKerne: {0}\n'.format(subprocess.getoutput('uname -rs'))
+        running += '\tProcessor: {0}\n'.format(subprocess.getoutput('cat /proc/cpuinfo | grep "model name" | tr -s " " | cut -d " " -f 3-'))
+        running += '\tRAM: {0}MB ({1}% used)\n'.format(int(psutil.virtual_memory()[0] / 1000 / 1000), psutil.virtual_memory()[2])
+        running += '\tPython: {0} ({1})\n'.format(str(platform.python_version()), str(platform.python_compiler()))
+        running += '\tServer time: {0}\n'.format(time.strftime("%c"))
+        running += '\tUptime: Server up for {0} days\n'.format(subprocess.getoutput('uptime').split()[2])
+        bot.send_message( cid, running, parse_mode="Markdown")
