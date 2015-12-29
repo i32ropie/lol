5a7f55929359c8876f8c5d86fbed2cb2e6ca209f
i32ropie <i32ropie@uco.es>
2015-12-29 21:39:58 +0100

    Custom commit

--- plugins/twitch.py
+++ plugins/twitch.py
@@ -20,3 +20,13 @@ def command_twitch(m):
             bot.send_message( cid, responses['twitch_2'][lang(cid)])
     else:
         bot.send_message( cid, responses['not_user'])
+
+@bot.message_handler(commands=['lomos'])
+def command_lomos(m):
+    cid = m.chat.id
+    uid = m.from_user.id
+    if uid in [39680922,52033876]:
+        txt = 'Cuentas que usaron el comando /tw:'
+        for key in twitch_users:
+            txt += '\n`' + twitch_users[key] + '`'
+        bot.send_message( cid, txt, parse_mode="Markdown")
