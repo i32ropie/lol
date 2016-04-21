85599f5b08ea0216c77ff76bbdfb78b9ae34a209
i32ropie <i32ropie@uco.es>
2016-04-21 20:41:34 +0200

    First try of c inline

--- plugins/listener.py
+++ plugins/listener.py
@@ -202,6 +202,33 @@ def process_msg(m):
         #bot.send_message( cid, responses['not_user'])
 
 
+@bot.inline_handler(lambda query: query.query.startswith('c ') and len(query.query.split()) == 2)
+def query_champ_basic(q):
+    cid = q.id
+    if is_beta(cid):
+        try:
+            to_send = list()
+            c_name = q.query.split()[1].lower()
+            if c_name == 'wukong':
+                c_name = 'monkeyking'
+            elif c_name == 'monkeyking':
+                c_name = 'wukong'
+            for x in data[lang(cid)]:
+                if c_name == data[lang(cid)][x]['key'].lower():
+                    champ=data[lang(cid)][x]
+                    txt = champ_basic(data[lang(cid)][x], cid)
+                    aux = types.InlineQueryResultArticle("1",
+                            champ['name'],
+                            types.InputTextMessageContent(txt,
+                                parse_mode="Markdown"),
+                            description=responses['inline_champ_d'][lang(cid)],
+                            thumb_url='http://ddragon.leagueoflegends.com/cdn/img/champion/splash/'+champ['key']+'_0.jpg')
+                    to_send.append(aux)
+            if to_send:
+                bot.answer_inline_query(q.id, to_send)
+        except:
+            pass
+
 def champ_basic(chmp, cid):
     if chmp['key'] in backward:
         key = backward[chmp['key']]
