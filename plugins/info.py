f5fbcfa6f6011cf1c613e675c7193f6217192b35
i32ropie <i32ropie@uco.es>
2016-05-23 16:56:56 +0200

    Minor update

--- plugins/info.py
+++ plugins/info.py
@@ -10,6 +10,8 @@ b1 = types.InlineKeyboardButton("Channel", url="https://telegram.me/league_of_le
 b2 = types.InlineKeyboardButton("Developer", url="https://telegram.me/edurolp")
 b3 = types.InlineKeyboardButton("GitHub", url="https://github.com/i32ropie/lol")
 markup.add(b1, b2, b3)
+b4 = types.InlineKeyboardButton("PayPal", url="https://paypal.me/edurolp")
+markup.add(b4)
 
 @bot.message_handler(func=lambda m: m.content_type ==
                      'text' and m.text in ['INFO', 'ИНФОРМАЦИЯ'])
@@ -30,7 +32,7 @@ def command_info(m):
         return None
     if is_user(cid):
         bot.send_chat_action(cid, 'typing')
-        bot.send_message(cid, responses['info'][lang(cid)], reply_markup=markup)
+        bot.send_message(cid, responses['info'][lang(cid)], reply_markup=markup, disable_web_page_preview=True)
     else:
         bot.send_chat_action(cid, 'typing')
         bot.send_message(cid, responses['not_user'])
