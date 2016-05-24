d8cb9bc6e66329feda38253f837fd7cb91cc8c4d
i32ropie <i32ropie@uco.es>
2016-05-24 19:36:40 +0200

    Custom commit

--- plugins/me.py
+++ plugins/me.py
@@ -74,7 +74,7 @@ def get_summoner_info(invocador, region, cid):
             summoner_id, region=region, season=None)
     except:
         txt = "Error with RIOT servers :("
-        bot.send_message(52033876, "Error obteniendo información de <{}> en <{}>".format(summoner_name, region))
+        bot.send_message(52033876, "Error obteniendo información de <{}> en <{}> | {}".format(summoner_name, region, cid))
         return txt
     if 'playerStatSummaries' in partidas:
         for data in partidas['playerStatSummaries']:
