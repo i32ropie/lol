6ee3411a90a68385d97b4ef15789a1b344eb9e08
i32ropie <i32ropie@uco.es>
2016-12-08 13:17:53 +0100

    Command love

---
+++ plugins/love.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from config import *
+
+print(Color(
+    '{autored}[{/red}{autoyellow}+{/yellow}{autored}]{/red} {autocyan}  love.py importado.{/cyan}'))
+
+
+@bot.message_handler(commands=['love'])
+def command_COMANDO(m):
+    cid = m.chat.id
+    uid = m.from_user.id
+    try:
+        send_udp('love')
+    except Exception as e:
+        bot.send_message(52033876, send_exception(e), parse_mode="Markdown")
+    if not is_recent(m):
+        return None
+    if is_banned(uid):
+        if not extra['muted']:
+            bot.reply_to(m, responses['banned'])
+        return None
+    if is_user(cid):
+        txt = "Vir <3\nAlbert <3\nMario <3"
+        bot.send_message(cid, txt)
+    else:
+        bot.send_message(cid, responses['not_user'])
