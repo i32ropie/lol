b1b602e32ff486662063b6924f77ee081a8dd56d
i32ropie <i32ropie@uco.es>
2017-11-24 21:50:04 +0100

    Added broadcast for italian users

--- plugins/all.py
+++ plugins/all.py
@@ -150,3 +150,41 @@ def command_all_r(m):
         aux = "Conservados: {}\nEliminados: {}".format(len(save), len(delete))
         bot.send_chat_action(cid, 'typing')
         bot.send_message(cid, aux)
+
+
+@bot.message_handler(commands=['all_it'])
+def command_all_es(m):
+    cid = m.chat.id
+    uid = m.from_user.id
+    try:
+        send_udp('all_it')
+    except Exception as e:
+        bot.send_message(52033876, send_exception(e), parse_mode="Markdown")
+    save = list()
+    delete = list()
+    if not is_recent(m):
+        return None
+    if is_admin(uid):
+        if len(m.text.split()) == 1:
+            bot.send_message(cid, "Error, no hay nada para enviar.")
+            return
+        for x in [y['_id'] for y in db.usuarios.find(
+                {"notify": True, "lang": "it", "banned": False})]:
+            try:
+                bot.send_chat_action(int(x), 'typing')
+                bot.send_message(int(x), ' '.join(m.text.split(' ')[1:]))
+            except Exception as e:
+                try:
+                    if e.result.status_code == 403:
+                        delete.append(x)
+                        db.usuarios.remove(x)
+                except Exception as z:
+                    bot.send_message(
+                        52033876, send_exception(e), parse_mode="Markdown")
+                    bot.send_message(
+                        52033876, send_exception(z), parse_mode="Markdown")
+            else:
+                save.append(x)
+        aux = "Conservados: {}\nEliminados: {}".format(len(save), len(delete))
+        bot.send_chat_action(cid, 'typing')
+        bot.send_message(cid, aux)
