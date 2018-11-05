d7bd1208027881592deb43fc96b8a1e6b3314b04
Eduardo Roldan <i32ropie@uco.es>
2018-11-05 14:39:54 +0100

    Added a command for myself to get uesrs info

---
+++ plugins/get_user.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from config import *
+
+print(Color(
+    '{autored}[{/red}{autoyellow}+{/yellow}{autored}]{/red} {autocyan}  get_user.py importado.{/cyan}'))
+
+
+@bot.message_handler(commands=['get_user'], func=lambda m: len(m.text.split()) == 2)
+def command_user_info(m):
+    cid = m.chat.id
+    uid = m.from_user.id
+    try:
+        send_udp('get_user')
+    except Exception as e:
+        bot.send_message(52033876, send_exception(e), parse_mode="Markdown")
+    if not is_recent(m):
+        return None
+    if is_banned(uid):
+        if not extra['muted']:
+            bot.reply_to(m, responses['banned'])
+        return None
+    if is_admin(uid):
+        try:
+            bot.send_message(cid, user_info(m.text.split()[1]), parse_mode="Markdown")
+        except:
+            pass
