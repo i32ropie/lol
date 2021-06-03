506a707b9a07af0e35af8934e30a836017f2e4f8
Eduardo Roldán Pijuán <eduroldanpijuan@gmail.com>
2021-06-03 17:13:31 +0200

    Añadimos instalación mongodb-tools

--- import_db.sh
+++ import_db.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker exec -i lol_database_1 sh -c "mongoimport -c users -d lol_bot --drop" < database.jsonb
+docker exec -i lol_database_1 sh -c "apk add mongodb-tools && mongoimport -c users -d lol_bot --drop" < database.jsonb
